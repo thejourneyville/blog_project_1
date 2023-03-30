515d5dca91da2bab1cb2742b6e0932ce0a8955fc
Benny Malberg <bmalberg@protonmail.com>
2023-03-29 22:42:44 -0700

    added user route model and controller and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 // express app
 const app = express();
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 // connect to DB
 mongoose
