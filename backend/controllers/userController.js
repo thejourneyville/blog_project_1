cd4a665d642517dadf5eb6f019f08b22d2a07bf6
Benny Malberg <bmalberg@protonmail.com>
2023-04-01 22:56:57 -0700

    added jwt creation using the .env secret and returning to frontend in signupUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,29 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
-}
+    res.json({ mssg: "login user" });
+};
 
 // signup user
 const signupUser = async (req, res) => {
-
     const { userName, password } = req.body;
 
     try {
-        const user = await User.signup(userName, password)
-        res.status(200).json({userName, user})
+        const user = await User.signup(userName, password);
 
+        // create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({ userName, token });
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message });
     }
-}
+};
 
-module.exports = { loginUser, signupUser }
+module.exports = { loginUser, signupUser };
